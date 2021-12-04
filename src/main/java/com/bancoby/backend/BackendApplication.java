8439f88e1127daa38cfd21502ff64ee8fe2bc03f
Jonathan Ferreira <jhouus33@gmail.com>
2021-12-03 21:48:34 -0300

    At pom e app.pro

--- src/main/java/com/bancoby/backend/BackendApplication.java
+++ src/main/java/com/bancoby/backend/BackendApplication.java
@@ -1,13 +1,23 @@
 package com.bancoby.backend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.bancoby.backend.models.Agencia;
+
 @SpringBootApplication
-public class BackendApplication {
+public class BackendApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Agencia ag1 = new Agencia("Rua doa bobos, 10", null);
+		
 	}
 
 }
